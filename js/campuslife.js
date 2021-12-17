e4781100788c55c979a19d391f3d9b90cf5b88b9
leokwo <rguo@uw.edu>
2021-12-17 16:24:56 -0500

    buddyexperience and fleamarket fix 1

--- js/campuslife.js
+++ js/campuslife.js
@@ -1,3 +1,63 @@
+let socket = new WebSocket("ws://127.0.0.1:8000/");
+
+var isBuddyExp = true;
+var campusLifeInfoArray = new Array();
+
+socket.onopen = function(e) {
+  socket.send('end');
+  loadCampusLife();
+};
+
+socket.onmessage = function(event) {
+  if (event.data != "campuslife" && event.data != "end") {
+    campusLifeInfoArray.push(event.data);
+  }
+  if (event.data == "end") {
+    addPosts(campusLifeInfoArray);
+  }
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    alert('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
+
+function loadCampusLife() {
+  if (isBuddyExp) {
+    socket.send("buddyexperience");
+  } else {
+    socket.send("fleamarket");
+  }
+}
+
+function addPosts(posts) {
+  $("#newPostCard").nextAll().remove();
+  for (let i = 0; i < posts.length; i+=3) {
+    $("#newPostCard").after(
+      "<hr>" +
+      "<a class='card-body userPosts' href='post.html'>" +
+        "<h5 class='card-title'>" + posts[i] + "</h5>" +
+        "<p class='timePosted' style='font-size:x-small'>" + posts[i + 2] + "</p>" +
+        "<p class='card-text'>"+ posts[i + 1] + "</p>" +
+      "</a>"
+    );
+  }
+}
+
+
+
+
+
+
+
+
 function tabOnclick() {
   const fleaMarketTab = $("#fleaMarketTab");
   const buddyExperienceTab = $("#buddyExperienceTab");
@@ -9,14 +69,16 @@ function tabOnclick() {
     buddyExperienceTab.addClass("active");
     pageTitle.html("BuddyExperience");
     bannerPhoto.css("background-image", "url('../img/experienceBannerPhoto.jpeg')");
+    isBuddyExp = true;
+    loadCampusLife();
   } else {
     fleaMarketTab.addClass("active");
     buddyExperienceTab.removeClass("active");
     pageTitle.html("FleaMarket");
     bannerPhoto.css("background-image", "url('../img/marketBannerPhoto.jpeg')");
+    isBuddyExp = false;
+    loadCampusLife();
   }
-
-  // TODO: Post switch when clicking button
 }
 
 function createNewPost() {
@@ -30,4 +92,26 @@ function closeNewPost() {
   $("#newPostCard").css("display", "none");
   $("#closeIcon").css("display", "none");
   $("#newPostIcon").css("display", "flex");
+}
+
+
+
+
+
+function newPostSubmit() {
+  const d = new Date();
+  if (isBuddyExp) {
+    socket.send("newBuddyExpPost");
+  } else {
+    socket.send("newFleaMartPost");
+  }
+  // socket.send("newPost");
+  // socket.send(courseId);
+  socket.send($("#newPostTitle").val());
+  socket.send($("#newPostTestArea").val());
+  socket.send(d.getFullYear().toString());
+  socket.send(d.getMonth().toString());
+  socket.send(d.getDate().toString());
+  alert("Submitted!");
+  window.location.href = "coursePage.html";
 }
